0cc3d2962d1422a96ecdb01fa849c2837108a22e
Bea <31886814+beatamatriz@users.noreply.github.com>
2024-01-27 19:00:16 +0100

    ready player one

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
 
-const speed : float = 180.0
-const jump_velocity : float = -200.0
-const double_jump_velocity : float = -150.0
+const speed : float = 600.0
+const jump_velocity : float = -666.0
+const double_jump_velocity : float = -599.0
 
 var can_double_jump : bool = false;
-
+var last_check_point
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -21,11 +21,22 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump"):
 		if (is_on_floor()):
+			$Animation.stop()
+			$Animation.play("jump")
 			velocity.y = jump_velocity
 		elif (can_double_jump):
+			$Animation.stop()
+			$Animation.play("jump")
 			velocity.y = double_jump_velocity;
 			can_double_jump = false;
-			
+	else:
+		if (is_on_floor()):
+			if velocity.x != 0:
+				$Animation.play("walk")
+			else:
+				$Animation.play("idle")
+		elif (velocity.y > 0):
+			$Animation.play("fall")			
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -34,5 +45,9 @@ func _physics_process(delta):
 		velocity.x = direction * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
-
+	
+	if velocity.x < 0:
+		$Sprite.set_flip_h(false)
+	elif velocity.x > 0:
+		$Sprite.set_flip_h(true)
 	move_and_slide()
