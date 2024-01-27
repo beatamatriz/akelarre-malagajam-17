f6af525cbda58add2fd7f0b240246a711bab846a
Vega <para.vega.pm@gmail.com>
2024-01-27 17:13:28 +0100

    celia map update
    celia map update

---
+++ Celia/prueba.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var player = $TileMap/Player
+@onready var starting_spawn = $StartingSpawn
+
+func _ready():
+	if CheckpointsGlobal.spawn:
+		player.global_position = CheckpointsGlobal.spawn
+	else:
+		player.global_position = starting_spawn.global_position
+
+func _process(delta):
+	if player.position.y > 300:
+		if CheckpointsGlobal.spawn:
+			player.global_position = CheckpointsGlobal.spawn
+		else:
+			player.global_position = starting_spawn.global_position
