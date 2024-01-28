fddf12506709a569aa6a9dd106f5224179c139cd
Bea <31886814+beatamatriz@users.noreply.github.com>
2024-01-28 13:53:02 +0100

    sexo 2

--- scenes/axon/axon.gd
+++ scenes/axon/axon.gd
@@ -3,14 +3,14 @@ extends Area2D
 var rng = RandomNumberGenerator.new()
 var my_random_number = int(rng.randf_range(1, 4))
 	
-func _on_body_entered(_body):
-	if my_random_number == 1:
-		$AudioStreamPlayer.play()
-	elif my_random_number == 2:
-		$AudioStreamPlayer2.play()
-	elif my_random_number == 3:
-		$AudioStreamPlayer3.play()
-	await get_tree().create_timer(0.25).timeout
-	#get_tree().quit()
-	print(get_tree().reload_current_scene())
+func _on_body_entered(body):
+	if body.name == "Player":
+		if my_random_number == 1:
+			$AudioStreamPlayer.play()
+		elif my_random_number == 2:
+			$AudioStreamPlayer2.play()
+		elif my_random_number == 3:
+			$AudioStreamPlayer3.play()
+		await get_tree().create_timer(0.25).timeout
+		#get_tree().quit()
 
--- scenes/checkpoint/checkpoint_script.gd
+++ scenes/checkpoint/checkpoint_script.gd
@@ -1,10 +1,17 @@
 extends Area2D
 
-@onready var animation_player = $AnimationPlayer
+var active
 
 func _ready():
-	animation_player.play("inactive")
+	active = false
+	$SpriteActive.set_visible(false)
+	$SpriteInactive.set_visible(true)
+	$Animation.play("inactive")
 
 func _on_body_entered(body):
-	CheckpointsGlobal.spawn = global_position
-	animation_player.play("active")
+	if not active and body.name == "Player":
+		active = true
+		CheckpointsGlobal.spawn = global_position
+		$SpriteInactive.set_visible(false)
+		$SpriteActive.set_visible(true)
+		$Animation.play("active")
