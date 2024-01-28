eaa2d1d6d6642e24b2875dbc35f803f1e460c394
Vega <para.vega.pm@gmail.com>
2024-01-28 11:53:13 +0100

    main mnue update

--- scenes/main_menu/main_menu_script.gd
+++ scenes/main_menu/main_menu_script.gd
@@ -2,5 +2,5 @@ extends Control
 
 var main_level = preload("res://scenes/level/level.tscn").instantiate()
 
-func _on_button_pressed():
+func _on_texture_button_pressed():
 	get_tree().root.add_child(main_level)
